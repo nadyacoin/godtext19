34a49d68b64eb3aa0c82dd9ea9569e2fd32ad82d
nadyacoin <103100190+nadyacoin@users.noreply.github.com>
2025-10-19 19:54:35 +0900

    Update text_trainer.py

--- scripts/text_trainer.py
+++ scripts/text_trainer.py
@@ -200,7 +200,7 @@ def main():
     )
     
     parser.add_argument(
-        "--reg-ratio", type=float, help="Reg ratio to use for training", default=1.14182
+        "--reg-ratio", type=float, help="Reg ratio to use for training", default=1.14
     )
     
     args = parser.parse_args()
